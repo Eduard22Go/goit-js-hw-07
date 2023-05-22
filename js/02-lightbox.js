fa8c089348882a418f31292c0148bedc32f81bb3
Eduard <mefoed@gmail.com>
2023-05-22 18:01:27 +0300

    02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,8 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
-
+// Створення розмітки
 const galleryContainer = document.querySelector('.gallery');
 
 createGalleryItemsMarkup();
